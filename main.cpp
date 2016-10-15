51c91e9dc4b4e47ce5ff5b614ec06a407b6b32f8
Luis Daniel Chaparro <luisdchaparro@outlook.com>
2016-10-14 20:00:19 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//+++++++++++++++++++++++++++++++++++++++++++
+// Author:   Luis D. Chaparro
+// Course:   CSCE 4600
+// Due Date: June 14th 2016
+// Description: Program that simulates the 
+//				creation of a processes.
+//+++++++++++++++++++++++++++++++++++++++++++
+#include "pcb.h"
+
+int main(){	
+	// Create new Process Table
+	process_Table p1;
+	
+	// Create PCB's to fill table
+	p1.create_PCB();
+	
+	
+	
+	// Print PCB's
+	//p1.print_Processes();
+	//p1.printAnalysis();
+	
+	return 0;
+}
